777fcf65580e6a58eb1f5309f226813654de1ae8
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-21 19:07:56 +0530

    Added async await with mongoose

--- Playground/promise-chaining.js
+++ Playground/promise-chaining.js
@@ -5,6 +5,7 @@ const User = require('../src/models/user')
 //5fb666a64ae9474e7ce9c682
 //
 
+/*
 User.findByIdAndUpdate('5fb666a64ae9474e7ce9c682', { age: 50 })
   .then((res, err) => {
     console.log(res)
@@ -12,4 +13,16 @@ User.findByIdAndUpdate('5fb666a64ae9474e7ce9c682', { age: 50 })
     return User.countDocuments({ age: 50 })
   })
   .then(res2 => console.log(res2))
+  .catch(err => console.log(err))
+*/
+
+// Refactoring into Async await
+const updateAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age })
+  const count = await User.countDocuments({ age })
+  return `There are ${count} users with age ${age} `
+}
+
+updateAndCount('5fb666a64ae9474e7ce9c682', 36)
+  .then(res => console.log(res))
   .catch(err => console.log(err))
--- Playground/promise-chaining2.js
+++ Playground/promise-chaining2.js
@@ -1,12 +1,25 @@
 require('../src/db/mongoose')
+const { findByIdAndDelete } = require('../src/models/task')
 const Task = require('../src/models/task')
 
 // Promise Chaining to find the task by ID and remove it then count incomplete tasks
-
+/*
 Task.findByIdAndDelete('5fb8e01d49273e81855cd7b7')
   .then(res => {
     console.log(res)
     return Task.countDocuments({ status: false })
   })
   .then(res2 => console.log(res2))
+  .catch(err => console.log(err))
+  */
+
+// Refactor with Async await
+const deleteTaskAndCount = async id => {
+  await Task.findByIdAndDelete(id)
+  const count = await Task.countDocuments({ status: false })
+  return console.log(`Incomplete tasks ${count}`)
+}
+
+deleteTaskAndCount('5fb90ae45e7f3c9d20808190')
+  .then(res => console.log(res))
   .catch(err => console.log(err))
