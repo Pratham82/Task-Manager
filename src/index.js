e0203f84e0409d42e4788a657d47fdf117dc832f
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-23 12:20:37 +0530

    Added bcrypt example

--- src/index.js
+++ src/index.js
@@ -13,3 +13,17 @@ app.use('/tasks', require('./routers/task'))
 
 // Starting up server
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
+
+const bcrypt = require('bcrypt')
+const myFunc = async password => {
+  // Hashing password
+  const hashedPass = await bcrypt.hash(password, 8)
+  console.log(password)
+  console.log(hashedPass)
+
+  // Comparing hashes
+  const matchedVal = await bcrypt.compare('Red12345', hashedPass)
+  console.log(matchedVal ? 'Passowrd Matched' : 'Passwords do not match')
+}
+
+myFunc('Red1234545')
