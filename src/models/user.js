7ba22de72c9405dae45d01395cc412376dd42bcb
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-23 12:16:09 +0530

    Added password hashing middleware

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcrypt')
 
-// Creating User model with validations
-const User = mongoose.model('User', {
+// Creating a seperate schema
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -40,4 +41,24 @@ const User = mongoose.model('User', {
     },
   },
 })
+
+// Setting up a middleware with userSchema
+
+userSchema.pre('save', async function (next) {
+  // Instead of using "this" everywhere we will declare it once
+  const user = this
+
+  // Hashing the password only if the passowrd is modified,  then save it to the DB
+  // isModified will be true when the use is first created and also when the user is being updated    and password was one of the updates
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+
+  console.log('Just before saving')
+  // "next" will run some code before the user is saved
+  next()
+})
+
+// Creating User model with validations
+const User = mongoose.model('User', userSchema)
 module.exports = User
