82565f0e829cf0c79a34b8f9f0a5f454d8901b10
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-23 21:27:12 +0530

    Added auth middlewares

---
+++ src/middlewares/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const auth = async (req, res, next) => {
+  try {
+    // Get the authorization token which is passed by the user
+    // If the header is not passed in the header then the error will be thrown
+    const token = req.header('Authorization').replace('Bearer ', '')
+
+    // Verify the token if its valid or not
+    const decoded = jwt.verify(token, 'secretKey')
+
+    // Find the User with deoced id
+    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+
+    if (!user) {
+      throw new Error()
+    }
+
+    // Passing the user value to the route handlers
+    req.user = user
+    next()
+
+    console.log(token)
+  } catch (err) {
+    res.status(401).send({ error: 'Please authenticate' })
+  }
+}
+
+module.exports = auth
