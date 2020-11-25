6ac25b08ca90ddaf7e21cd17cc8c84e96e9b00f9
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-25 16:13:17 +0530

    Added virtual property exaple

--- src/index.js
+++ src/index.js
@@ -34,3 +34,21 @@ app.use('/tasks', require('./routers/task'))
 
 // Starting up server
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
+
+const Task = require('./models/task')
+const User = require('./models/user')
+const main = async () => {
+  // Find user by the task
+  //const task = await Task.findById('5fbe2ece3ff5b93f5e7c6d85')
+  // Populat will take the ID of the owner to being the entire profile of that owner
+  //await task.populate('owner').execPopulate()
+  //console.log(task.owner)
+
+  //Find User by their ID
+  //
+  const user = await User.findById('5fbe2c474e90e03b2b3ebc7a')
+  await user.populate('tasks').execPopulate()
+  console.log(user.tasks)
+}
+
+main()
