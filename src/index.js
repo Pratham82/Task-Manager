ebea0c947f312e58b23e04fa63b0807c4757e775
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-27 13:18:01 +0530

    Installed multer and added example

--- src/index.js
+++ src/index.js
@@ -4,33 +4,28 @@ require('./db/mongoose')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// File upload in express
+const multer = require('multer')
+const upload = new multer({
+  dest: 'images',
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, callback) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return callback(new Error('Please upload a Word documenr'))
+    }
+
+    callback(undefined, true)
+  },
+})
+
+//app.post('/upload', upload.single('upload'), (req, res) => res.send())
+
 // Parsing the JSON using express
 app.use(express.json())
-
-// Register a new middleware function
-// Middleware methods will be run before it goes to the route handlers
-
-/*app.use((req, res, next) => {
-  // We can filter the routes which users can acccess
-  if (req.method === 'GET') {
-    //GET requests should be private
-    res.send({ message: 'GET Requests are disabled' })
-  } else {
-    next()
-  }
-})*/
-
-/*
-// Creating a middleware for maintenance mode
-app.use((req, res, next) => {
-  // Dont allow any requests in the maintenance
-  res.status(503).send('Site is under maintenance mode')
-})
-*/
 
 // Redirecting the requests to the specific routers
 app.use('/users', require('./routers/user'))
 app.use('/tasks', require('./routers/task'))
-
-// Starting up server
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
